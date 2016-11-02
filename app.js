713e3fe27de38706076d84fd47980d3699fc0a21
Alexandrea Defreitas <alexandreadef@gmail.com>
2016-11-02 17:53:46 -0400

    Connect to Plaid api

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+var fetch = require('node-fetch');
+
+fetch(`https://tartan.plaid.com/connect?public_key=${process.env.PLAID_KEY}&client_id=${process.env.PLAID_CLIENT}&secret=${process.env.PLAID_SECRET}&type=${process.env.TYPE}&username=${process.env.USERNAME}&password=${process.env.PASSWORD}`, {
+  method: 'post',
+})
+.then(function(res) {
+  return res.json();
+}).then(function(json) {
+  console.log(json);
+});
