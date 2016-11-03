06f7b485f80284a50c6a1ede6436cd0189a43cdb
Alexandrea Defreitas <alexandreadef@gmail.com>
2016-11-03 10:21:54 -0400

    Renders plaid data

--- app/components/app/index.js
+++ app/components/app/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getPlaidData } from '../../util/plaid.service.js';
 
-export function app(props) {
-  return (
-    <div className="root">
-      <h1>Cents</h1>
-    </div>
-  );
+export class app extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: [],
+      transactions: [],
+    }
+  }
+
+  componentWillMount() {
+    getPlaidData().then(res => {
+
+      this.setState({
+        accounts: res.accounts,
+        transactions: res.transactions,
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div className="root">
+        <h1>Cents</h1>
+        <div className="account">
+          <h2>Accounts</h2>
+          {
+            this.state.accounts.map((account, i) => (
+              <div>
+                <p>{account.meta.name}, <small>{account.type}</small></p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default app;
