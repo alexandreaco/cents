aaf649281d67867fe8c41310d133ada7b99ebbb4
Alexandrea Defreitas <alexandreadef@gmail.com>
2016-12-13 16:10:31 -0500

    [Feature] Get calendar data from global state

--- app/components/Calendar/index.jsx
+++ app/components/Calendar/index.jsx
@@ -8,28 +8,21 @@ import Week from './Week';
 const moment = require('moment');
 
 class Calendar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      transactionsByDay: mapTransactions(getDaysPast(600), this.props.user.transactions),
-    };
-  }
-
   componentWillMount() {
     const { dispatch } = this.props;
     dispatch(getAllTransactions());
   }
 
   render() {
-    const { user } = this.props;
+    const { allTransactions, transactionsByDay } = this.props;
     return (
       <div className={styles.root}>
         <h2>Calendar</h2>
         <div className={styles.calendar}>
           {
-            Object.keys(this.state.transactionsByDay).map((day, i) => (
+            getDaysPast(1000).map((day, i) => (
               moment(day).weekday() === 0 && (
-                <Week startDay={new Date(day)} key={i} transactions={mapTransactions(getWeek(day), user.transactions)} />
+                <Week startDay={new Date(day)} key={i} transactions={mapTransactions(getWeek(day), allTransactions)} />
               )
             ))
           }
@@ -42,14 +35,12 @@ class Calendar extends Component {
 // Retrieve data from store as props
 const mapStateToProps = (state) => {
   return {
-    user: state.app.user,
+    allTransactions: state.transactions.all,
   };
 };
 
 Calendar.propTypes = {
-  user: PropTypes.shape({
-    transactions: PropTypes.array,
-  }),
+  allTransactions: PropTypes.array.isRequired,
   dispatch: PropTypes.func.isRequired,
 };
 
